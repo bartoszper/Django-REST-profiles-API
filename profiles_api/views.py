f4e2e73e272213c33a592779eef219450c74f388
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-27 18:18:06 +0100

    Add aspi methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import HelloSerializer
+from rest_framework import status
+
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = HelloSerializer
 
     def get(self, request, format=None):
         """Return a list of APIView features"""
@@ -17,5 +21,37 @@ class HelloApiView(APIView):
             "message":"Hello",
             "an_apiview": an_apiview
         })
+
+    def post(self, request):
+        """Create a hello message with our name"""
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!!!'
+            return Response({
+                'message':message
+            })
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({
+            'Method':'PUT'
+        })
+
+    def patch(self, request, pk=None):
+        return Response({
+            'method':'PATCH'
+        })
+    
+    def delete(self, request, pk=None):
+        return Response({
+            'method':'DELETE'
+        })
+
+
 
     
