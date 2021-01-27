857f0c87d93c2bbe8e00ef8755a95fbc117bfe9e
Bartosz Perenc <bartosz.perenc@gmail.com>
2021-01-27 19:11:17 +0100

    Add router

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 
 urlpatterns = [
-    path('hello-view/',views.HelloApiView.as_view())
+    path('hello-view/',views.HelloApiView.as_view()),
+    path('',include(router.urls))
 ]
